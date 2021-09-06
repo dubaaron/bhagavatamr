4a79f6cfe41cdeb5b838b184e602305b875f8077
haribol aaron w <aaron@adubsvc.net>
2021-09-05 20:27:32 -0600

    Add code to fetch raw html; begin encapsulation in functions

--- bhagavatamr.rb
+++ bhagavatamr.rb
@@ -1,31 +1,75 @@
-puts "Jagat, Haribol!"
 
-# open file
+# - parse args, get canto and chapter (and destination?)
+# - turn off dictionary links - send req to:
+#   https://prabhupadabooks.com/php/data_ajax.php?action=changeSetting&encoding=unicode&dictionaryLinks=no&booksv=yes&bookss=yes&bookst=yes&booksp=yes
+# - 
+# - fetch bhagavatam html from https://prabhupadabooks.com/sb/<canto>/<chapter>?d=1
 
-if ARGV.empty?
-    puts "Need input file"
-    exit
+
+
+
+
+
+def fetch_chapter(canto = 1, chapter = 1)
+    require 'net/http'
+    require 'rubygems'
+    require 'mechanize'
+
+    url_to_switch_links_off = URI('https://prabhupadabooks.com/php/data_ajax.php?action=changeSetting&encoding=unicode&dictionaryLinks=no&booksv=yes&bookss=yes&bookst=yes&booksp=yes')
+
+    agent = Mechanize.new
+
+    print "Setting links to 'off' ... "
+
+    page = agent.get(url_to_switch_links_off)
+
+    puts page.body + "\n"
+
+    chapter_url = URI("https://prabhupadabooks.com/sb/#{canto}/#{chapter}?d=1")
+
+    puts "Fetching Śrīmad-Bhāgavatam Canto %02d, Chapter %02d" % [canto, chapter]
+    
+    # chapter_raw_html = Net::HTTP.get(chapter_url)
+    page = agent.get(chapter_url)
+
+    p page.body
+
+    require 'fileutils'
+    FileUtils.mkdir_p "./output"
+
+    output_file_name = "output/%02d.%02d_raw.html" % [canto, chapter]
+    File.write(output_file_name, page.body)
+    
 end
 
-# turn off dictionary links:
-# https://prabhupadabooks.com/php/data_ajax.php?action=changeSetting&encoding=unicode&dictionaryLinks=no&booksv=yes&bookss=yes&bookst=yes&booksp=yes
 
-file_to_open = ARGV[0]
+def extract_translations_from_markdown(full_markdown_file = ARGV[0])
 
-puts "Attempting to open #{file_to_open} ..."
+    puts "Attempting to open #{full_markdown_file} ..."
 
-File.open(file_to_open, 'r') do |input_file|
-    input_text = input_file.read()
+    File.open(full_markdown_file, 'r') do |input_file|
+        input_text = input_file.read()
 
-    regex = /TEXTS? (\d+(\D\d+)?)[\s\S]*?TRANSLATION\n\n([\S\s]*?)\n\n(PURPORT)?[\s\S]*?(?=(TEXT)|(Thus end the Bhaktivedanta))/m
+        regex = /TEXTS? (\d+(\D\d+)?)[\s\S]*?TRANSLATION\n\n([\S\s]*?)\n\n(PURPORT)?[\s\S]*?(?=(TEXT)|(Thus end the Bhaktivedanta))/m
 
-    translations_only = input_text.gsub(regex, '[\1.] \3' << "\n\n");
+        translations_only = input_text.gsub(regex, '[\1.] \3' << "\n\n");
 
-    p translations_only;
+        p translations_only;
 
-    output_file_name = file_to_open + ", translations.md"
-    File.open(output_file_name, 'w') do |output_file|
-        output_file.puts(translations_only)
-        puts "Wrote output to '#{output_file_name}'"
+        output_file_name = full_markdown_file + ", translations.md"
+        File.open(output_file_name, 'w') do |output_file|
+            output_file.puts(translations_only)
+            puts "Wrote output to '#{output_file_name}'"
+        end
     end
+end
+
+
+puts "Jagat, Haribol!"
+
+
+if ARGV.empty?
+    fetch_chapter()
+else
+    extract_translations_from_markdown(ARGV[0])
 end
