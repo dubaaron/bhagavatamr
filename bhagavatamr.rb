bd66e01f4972a57913f14a67343f4ddd47d849eb
Aaron Wallentine <aaron@adubsvc.net>
2022-04-29 01:51:09 -0600

    Fix indentation to 2 spaces

--- bhagavatamr.rb
+++ bhagavatamr.rb
@@ -9,59 +9,58 @@
 
 
 
-
 def fetch_chapter(canto = 1, chapter = 1)
-    require 'net/http'
-    require 'rubygems'
-    require 'mechanize'
+  require 'net/http'
+  require 'rubygems'
+  require 'mechanize'
 
-    url_to_switch_links_off = URI('https://prabhupadabooks.com/php/data_ajax.php?action=changeSetting&encoding=unicode&dictionaryLinks=no&booksv=yes&bookss=yes&bookst=yes&booksp=yes')
+  url_to_switch_links_off = URI('https://prabhupadabooks.com/php/data_ajax.php?action=changeSetting&encoding=unicode&dictionaryLinks=no&booksv=yes&bookss=yes&bookst=yes&booksp=yes')
 
-    agent = Mechanize.new
+  agent = Mechanize.new
 
-    print "Setting links to 'off' ... "
+  print "Setting links to 'off' ... "
 
-    page = agent.get(url_to_switch_links_off)
+  page = agent.get(url_to_switch_links_off)
 
-    puts page.body + "\n"
+  puts page.body + "\n"
 
-    chapter_url = URI("https://prabhupadabooks.com/sb/#{canto}/#{chapter}?d=1")
+  chapter_url = URI("https://prabhupadabooks.com/sb/#{canto}/#{chapter}?d=1")
 
-    puts "Fetching Śrīmad-Bhāgavatam Canto %02d, Chapter %02d" % [canto, chapter]
-    
-    # chapter_raw_html = Net::HTTP.get(chapter_url)
-    page = agent.get(chapter_url)
+  puts "Fetching Śrīmad-Bhāgavatam Canto %02d, Chapter %02d" % [canto, chapter]
+  
+  # chapter_raw_html = Net::HTTP.get(chapter_url)
+  page = agent.get(chapter_url)
 
-    p page.body
+  p page.body
 
-    require 'fileutils'
-    FileUtils.mkdir_p "./output"
+  require 'fileutils'
+  FileUtils.mkdir_p "./output"
 
-    output_file_name = "output/%02d.%02d_raw.html" % [canto, chapter]
-    File.write(output_file_name, page.body)
+  output_file_name = "output/%02d.%02d_raw.html" % [canto, chapter]
+  File.write(output_file_name, page.body)
     
 end
 
 
 def extract_translations_from_markdown(full_markdown_file = ARGV[0])
 
-    puts "Attempting to open #{full_markdown_file} ..."
+  puts "Attempting to open #{full_markdown_file} ..."
 
-    File.open(full_markdown_file, 'r') do |input_file|
-        input_text = input_file.read()
+  File.open(full_markdown_file, 'r') do |input_file|
+    input_text = input_file.read()
 
-        regex = /TEXTS? (\d+(\D\d+)?)[\s\S]*?TRANSLATION\n\n([\S\s]*?)\n\n(PURPORT)?[\s\S]*?(?=(TEXT)|(Thus end the Bhaktivedanta))/m
+    regex = /TEXTS? (\d+(\D\d+)?)[\s\S]*?TRANSLATION\n\n([\S\s]*?)\n\n(PURPORT)?[\s\S]*?(?=(TEXT)|(Thus end the Bhaktivedanta))/m
 
-        translations_only = input_text.gsub(regex, '[\1.] \3' << "\n\n");
+    translations_only = input_text.gsub(regex, '[\1.] \3' << "\n\n");
 
-        p translations_only;
+    p translations_only;
 
-        output_file_name = full_markdown_file + ", translations.md"
-        File.open(output_file_name, 'w') do |output_file|
-            output_file.puts(translations_only)
-            puts "Wrote output to '#{output_file_name}'"
-        end
+    output_file_name = full_markdown_file + ", translations.md"
+    File.open(output_file_name, 'w') do |output_file|
+      output_file.puts(translations_only)
+      puts "Wrote output to '#{output_file_name}'"
     end
+  end
 end
 
 
@@ -82,7 +81,7 @@ puts "Jagat, Haribol!"
 
 
 if ARGV.empty?
-    fetch_chapter(canto: 3, chapter: 25)
+  fetch_chapter(canto: 3, chapter: 25)
 else
-    extract_translations_from_markdown(ARGV[0])
+  extract_translations_from_markdown(ARGV[0])
 end
